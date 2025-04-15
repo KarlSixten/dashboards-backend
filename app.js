8e3ed8ce53ba3e4df385b37e009c75bd8ee6da2e
Karl Sixten Bjarnø <karlsixten@gmail.com>
2025-04-15 14:10:31 +0200

    Added splat endpoint to serve SSR-page

--- app.js
+++ app.js
@@ -9,5 +9,13 @@ app.use(cors());
 import router from './routers/dashboardRouter.js';
 app.use(router);
 
+import path from 'path';
+
+app.use(express.static(path.resolve('../dashboards-frontend/dist/')));
+
+app.get("/{*splat}", (req, res) => {
+  res.sendFile(path.resolve('../dashboards-frontend/dist/index.html'));
+});
+
 const PORT = 8080;
 app.listen(PORT, () => console.log('Server is running on port:', PORT));
